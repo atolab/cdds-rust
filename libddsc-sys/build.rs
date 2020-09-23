844686ab5dad2878393f47c259f70cd9e00cfcc7
Esteve Fernandez <esteve@apache.org>
2020-09-23 16:16:59 +0200

    Build cyclonedds statically if not found (#9)
    * Added cyclonedds
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Embed CycloneDDS
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Fetch submodules
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Add OpenSSL dependency
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * Disable SSL
    
    Signed-off-by: Esteve Fernandez <esteve@apache.org>
    
    * checkin
    
    * Added features. Clean up CI. Added more targets to CI
    
    * Renamed job
    
    * Fix syntax
    
    * Fix syntax
    
    * Fix syntax 2
    
    * Install LLVM
    
    * Fix target
    
    * Rexport features
    
    * Static flag
    
    * Link against OpenSSL libraries
    
    * Update authors
    
    * Moved examples to cdds-util and cdds-util-rs
    
    * Fix cargo check call
    
    * Fix cargo test call

--- libddsc-sys/build.rs
+++ libddsc-sys/build.rs
@@ -1,17 +1,55 @@
 extern crate bindgen;
+extern crate cmake;
 
+use cmake::Config;
 use std::env;
+use std::path::Path;
 use std::path::PathBuf;
+use std::process::Command;
+
+fn build_static_cyclonedds() -> String {
+    if !Path::new("src/cyclonedds/.git").exists() {
+        let _ = Command::new("git")
+            .args(&["submodule", "update", "--init", "src/cyclonedds"])
+            .status();
+    }
+
+    let dst = Config::new("src/cyclonedds")
+        .define("BUILD_IDLC", "OFF")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .build();
+
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=ddsc");
+
+    // Link against OpenSSL libraries
+    println!("cargo:rustc-link-lib=ssl");
+    println!("cargo:rustc-link-lib=crypto");
+
+    format!("-I{}/include", dst.display())
+}
 
 fn main() {
-    println!("cargo:rustc-link-lib=ddsc");
-    println!("cargo:rustc-link-lib=cdds-util");
-    let bindings = bindgen::Builder::default()
+    let bindgen_builder = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-I/usr/local/include")
-        .generate_comments(false)
-        .generate()
-        .expect("Unable to generate bindings");
+        .generate_comments(false);
+
+    let bindings = if cfg!(feature = "static") {
+        bindgen_builder.clang_arg(build_static_cyclonedds())
+    } else {
+        println!("cargo:rustc-link-lib=ddsc");
+        println!("cargo:rustc-link-search=/opt/ros/foxy/lib/x86_64-linux-gnu");
+        println!("cargo:rustc-link-search=/opt/ros/eloquent/lib");
+        println!("cargo:rustc-link-search=/opt/ros/dashing/lib");
+        bindgen_builder
+            .clang_arg("-I/usr/local/include")
+            .clang_arg("-I/opt/ros/foxy/include")
+            .clang_arg("-I/opt/ros/eloquent/include")
+            .clang_arg("-I/opt/ros/dashing/include")
+            .clang_arg("-I/usr/lib/gcc/x86_64-linux-gnu/8/include")
+    }
+    .generate()
+    .expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
